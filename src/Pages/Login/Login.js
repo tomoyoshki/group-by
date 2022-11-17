bd424dd88c43c5a6124d1064207e4ad323c8dc4d
Tommy Kimura <tkimura4@illinois.edu>
2022-11-16 18:49:12 -0600

    Initialized skeleton

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 
-export default function Login() {
+async function loginUser(credentials) {
+    return "some_token"
+}
+
+export default function Login({setToken}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token)
+    }
     return(
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input type="text" onChange={e => setUsername(e.target.value)}/>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={e=> setPassword(e.target.value)}/>
                 </label>
                 <div>
                     <button type="submit">Submit</button>

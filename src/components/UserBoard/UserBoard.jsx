f3660bc1276b67b063aa371479eec6237f28a171
Tommy Kimura <tkimura4@illinois.edu>
2022-12-12 16:58:28 -0600

    Added request

--- src/components/UserBoard/UserBoard.jsx
+++ src/components/UserBoard/UserBoard.jsx
@@ -1,7 +1,7 @@
 import "./UserBoard.scss"
 import { getToken, removeToken } from "../../utils/useToken"
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function UserBoard() {
@@ -23,7 +23,28 @@ export default function UserBoard() {
                 navigate("/login")
             }
 
-            const user_data = res.data.data[0]
+            const request_ids = res.data.data[0].recevied_request_ids
+            var local_in_messages = [{
+                from_user: "me",
+                assignment_id: 'some_id'
+            }]
+            request_ids.forEach(async (element) => {
+                var p = {
+                    _id: element
+                }
+                var res = await axios.get("http://localhost:4000/api/requests", {
+                    params: {
+                        where: JSON.stringify(p)
+                    }
+                })
+                if (res.status === 404) return
+                var data = res.data.data
+                local_in_messages.push({
+                    from_user: data.user_send_request,
+                    assignment_id: data.assignment_id
+                })
+            })
+            setInMessage(local_in_messages)
         } catch(e) {
             console.log(e)
         }
@@ -36,6 +57,10 @@ export default function UserBoard() {
     useEffect(()=>{
         getUser()
     }, [])
+
+    useEffect(()=>{
+        console.log(in_message)
+    }, [in_message])
     return (
         <div className="user_board">
             <div className="user_section">
@@ -47,10 +72,17 @@ export default function UserBoard() {
             </div>
             </div>
             <div className="message_section">
-                <div className="message_title">Incomming Messages</div>
-            </div>
-            <div className="message_section">
-                <div className="message_title">Response Messages</div>
+                <div className="message_title">Incomming Requests</div>
+                {
+                    in_message.map(element=> {
+                        console.log(element)
+                        return (
+                            <Link className="message">
+                                Assignment: {element.assignment_id}
+                            </Link>
+                        )
+                    })
+                }
             </div>
         </div>
     )
